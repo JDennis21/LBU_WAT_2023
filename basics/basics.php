2fa58c8eca705409edf75cbe312e97230d857936
Josh <Josh@Vivobook>
2023-10-13 16:04:57 +0100

    Week 3 complete and up to date watIndex.html

--- basics/basics.php
+++ basics/basics.php
@@ -34,10 +34,24 @@
 <?php
     $num1 = 9;
     $num2 = 12;
+
     echo "num1 = $num1<br/>";
     echo "num2 = $num2<br/>";
     echo "num1 x num2 = " .($num1*$num2) ."<br/>";
     echo "num1 as a percentage of num2 = " .number_format(($num1/$num2)*100) ."%<br/>";
+    echo "num2 divided by num1 = " .number_format($num1/$num2) ." remainder " .$num2%$num1;
+?>
+<br/>
+<?php
+    $name = "David";
+    $age = 12;
+    $meters = 1.8;
+    $inches = ($meters*100)/2.54;
+
+    echo "<br/>Name: " . $name ."<br/>";
+    echo "<br/>Age: " . $age . "<br/>";
+    echo "<br/>Height in meters: " . $meters ."<br/>";
+    echo "<br/>Height in Feet and inches: " .floor($inches/12) ."ft " .($inches%12) ." ins<br/>";
 ?>
 </body>
 </html>
