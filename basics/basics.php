d7081897ac27b23a0a38f9ca03ec23d53e8c1426
Josh <Josh@Vivobook>
2023-10-13 13:22:12 +0100

    Started Php work

--- basics/basics.php
+++ basics/basics.php
@@ -1,2 +1,43 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+    <title>Basics of PHP</title>
+</head>
+<body>
 <?php
-
+    echo 'Josh Dennis<br/>'; //My first PHP.
+    echo 'c3641149<br/>';
+?>
+<h2>Using Escape Characters</h2>
+<?php
+    echo "\"most programmers say it's better to use 'echo' rather than 'print'\" says who?";
+?>
+<h2>Variables</h2>
+<?php
+    $name = "Josh";
+    $age = 20;
+    echo "Hi my name is " .$name .", and I am " .$age ." years old.</br>";
+    echo "Mi nombre es $name, y tengo $age anos de edad";
+?>
+<h2>Functions</h2>
+<?php
+    //gettype()returns the variable type
+    echo gettype($name);
+    echo '<br/>';
+    //strlen() returns the length of the string
+    echo strlen($name);
+    echo '<br/>';
+    //strtoUpper()returns the string in full caps
+    echo strtoUpper($name);
+?>
+<h2>Arithmetic</h2>
+<?php
+    $num1 = 9;
+    $num2 = 12;
+    echo "num1 = $num1<br/>";
+    echo "num2 = $num2<br/>";
+    echo "num1 x num2 = " .($num1*$num2) ."<br/>";
+    echo "num1 as a percentage of num2 = " .number_format(($num1/$num2)*100) ."%<br/>";
+?>
+</body>
+</html>
