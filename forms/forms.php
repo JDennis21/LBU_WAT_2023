c48711025271e77208ec8e317ade7855b413fdcb
Josh <Josh@Vivobook>
2023-10-19 23:19:48 +0100

    Completed session 4 extension

--- forms/forms.php
+++ forms/forms.php
@@ -17,8 +17,9 @@
     <h1>Processing Input from HTML Forms</h1>
     <h2>Login Form using GET</h2>
 
+    <!-- First Form -->
     <form method="get" action="forms.php">
-        <fieldset>
+        <fieldset class="formFit">
             <legend>
                 Login
             </legend>
@@ -35,8 +36,10 @@
         echo "Email: " . $_GET["txtEmail"] . " Password: " . $_GET["txtPass"];
     }
     ?>
+
+    <!-- Second Form -->
     <form method="post" action="forms.php">
-        <fieldset>
+        <fieldset class="formFit">
             <legend>Comments</legend>
             <label for="email1">Email: </label>
             <input type="text" name="txtEmail1" id="email1" value="<?php
@@ -58,7 +61,7 @@
     </form>
     <?php
     if (isset($_POST["loginSubmit1"])) {
-        $email = trim(filter_var($_POST["txtEmail1"], FILTER_SANITIZE_EMAIL));
+        $email = filter_var(trim($_POST["txtEmail1"], FILTER_SANITIZE_EMAIL));
         $comment = $_POST["txtComment"];
         if (!empty($email && filter_var($email, FILTER_VALIDATE_EMAIL))) {
             if (isset($_POST["checkbox"])) {
@@ -73,6 +76,108 @@
             echo "Email is not valid";
         } else {
             echo "Email must not be empty";
+        }
+    }
+    ?>
+    <!-- Third Form -->
+    <?php
+    echo "<h2 >Order Form</h2>";
+    echo "<p>Please fill out this form to place your order<br/></p>";
+    function clearVariables(): void
+    {
+       unset($_POST["pizzaSize"], $_POST["txtUsr"], $_POST["txtPizzaEmail"], $_POST["toppings"],
+       $_POST["capers"], $_POST["olives"], $_POST["parmesan"], $_POST["clear"], $_POST["submit"]);
+    }
+    if (isset($_POST["clear"])) clearVariables();
+    ?>
+    <form method="post" action="forms.php">
+        <fieldset id="orderField1">
+            <legend>
+                Enter your login details
+            </legend>
+            <label for="username">Username: </label>
+            <input type="text" name="txtUsr" id="username"
+                   value="<?php if (isset($_POST["txtUsr"])) echo trim($_POST["txtUsr"]); ?>"/>
+            <label for="pizzaEmail">Email: </label>
+            <input type="text" name="txtPizzaEmail" id="pizzaEmail"
+                   value="<?php if (isset($_POST["txtPizzaEmail"])) echo filter_var(trim($_POST["txtPizzaEmail"]),
+                       FILTER_SANITIZE_EMAIL);?>"/><br/>
+        </fieldset>
+        <fieldset id="orderField2">
+            <legend>
+                Pizza Selection
+            </legend>
+            <br/>
+            Size:
+            <input type="radio" name="pizzaSize" id="small" value="small"
+                <?php if (isset($_POST["pizzaSize"]) && $_POST["pizzaSize"]=="small") echo "checked";?>/>
+            <label for="small">small</label>
+            <input type="radio" name="pizzaSize" id="medium" value="medium"
+                <?php if (isset($_POST["pizzaSize"]) && $_POST["pizzaSize"]=="medium") echo "checked";?>/>
+            <label for="medium">medium</label>
+            <input type="radio" name="pizzaSize" id="large" value="large"
+                <?php if (isset($_POST["pizzaSize"]) && $_POST["pizzaSize"]=="large") echo "checked";?>/>
+            <label for="large">large</label>
+            <br/>
+            <br/>
+            <label for="toppings">Toppings:</label>
+            <select id="toppings" name="toppings">
+                <option value="please select"
+                    <?php if (isset($_POST["toppings"]) && $_POST["toppings"]=="please select") echo "selected";?>
+                >Please Select</option>
+                <option value="cheese"
+                    <?php if (isset($_POST["toppings"]) && $_POST["toppings"]=="cheese") echo "selected";?>
+                >Cheese</option>
+                <option value="pepperoni"
+                    <?php if (isset($_POST["toppings"]) && $_POST["toppings"]=="pepperoni") echo "selected";?>
+                >Pepperoni</option>
+            </select>
+            <br/>
+            <br/>
+            Extras:
+            <input type="checkbox" name="parmesan" id="parmesan" value = parmesan
+                <?php if (isset($_POST["parmesan"]) && $_POST["parmesan"]=="parmesan") echo "checked";?>/>
+            <label for="parmesan">Parmesan</label>
+            <input type="checkbox" name="olives" id="olives" value="olives"
+                <?php if (isset($_POST["olives"]) && $_POST["olives"]=="olives") echo "checked";?>/>
+            <label for="olives">Olives</label>
+            <input type="checkbox" name="capers" id="capers" value="capers"
+                <?php if (isset($_POST["capers"]) && $_POST["capers"]=="capers") echo "checked";?>/>
+            <label for="capers">Capers</label>
+            <br/>
+            <br/>
+        </fieldset>
+        <input type="submit" value="Submit" name="pizzaSubmit" id="pizzaSubmit"/>
+        <input type="submit" value="Clear" name="clear" id="pizzaClear"/>
+
+    </form>
+    <?php
+    if (isset($_POST["pizzaSubmit"])) {
+        $pizzaEmail = filter_var(trim($_POST["txtPizzaEmail"]), FILTER_SANITIZE_EMAIL);
+        $pizzaUser = trim($_POST["txtUsr"]);
+
+        if (empty($pizzaUser)) {
+            echo "Username is required";
+        } elseif (empty($pizzaEmail)) {
+            echo "Email is required";
+        } elseif (!filter_var($pizzaEmail, FILTER_VALIDATE_EMAIL)){
+            echo "Email is not Valid";
+        } elseif (!isset($_POST["toppings"]) || !isset($_POST["pizzaSize"])) {
+            echo "Must select a size and topping";
+        } else {
+            $order = ucfirst($_POST["pizzaSize"]) . " " . ucfirst($_POST["toppings"]);
+            $extras = "";
+
+            if (isset($_POST["parmesan"])) $extras .= "Parmesan ";
+            if (isset($_POST["olives"])) $extras .= "Olives ";
+            if (isset($_POST["capers"])) $extras .= "Capers ";
+            if (empty($extras)) $extras = "None";
+
+            echo "<h2>Thank you for your order:</h2>";
+            echo "CustomerID: " . "<strong>" . $_POST["txtUsr"] . "</Strong><br/>";
+            echo "Email: " . "<strong>" . $_POST["txtPizzaEmail"] . "</Strong><br/>";
+            echo "Your order: " . "<strong>" . $order . "</Strong><br/>";
+            echo "Extra toppings: " . "<strong>" . $extras . "</Strong><br/>";
         }
     }
     ?>
