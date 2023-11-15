4ee0f8480a8c25c75d97901fc97a41b5b27eca7b
Josh <Josh@Vivobook>
2023-11-15 14:08:42 +0000

    Completed portfolio task 1

---
+++ portfolio/register/insertRecord.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Register</title>
+    <link type="text/css" rel="stylesheet" href="../../style/main.css" />
+</head>
+<header>
+    <h1>Josh Dennis c3641149</h1>
+</header>
+<body>
+<?php
+include 'connection.php';
+global $connection;
+
+function trimString($string): string
+{
+    return htmlspecialchars(stripcslashes(trim($string)));
+}
+
+$_SESSION["username"] = trimString($_POST["txtUsername"]);
+$_SESSION["email"] = trimString($_POST["txtEmail"]);
+$_SESSION["age"] = trimString($_POST["selectAge"]);
+if (isset($_POST["checkConditions"])) {
+    $_SESSION["conditions"] = trimString($_POST["checkConditions"]);
+}
+function checkNotEmpty(): bool
+{
+    $valid = true;
+
+    if (empty(trimString($_POST["txtUsername"]))) {
+        $_SESSION["nameErr"] = "Username is required";
+        $valid = false;
+    }
+    if (empty(filter_var(trimString($_POST["txtEmail"]), FILTER_SANITIZE_EMAIL))) {
+        $_SESSION["emailErr"] = "Email is required";
+        $valid = false;
+    }
+    if (empty(trimString($_POST["txtPass"]))) {
+        $_SESSION["passErr"] = "Password is required";
+        $valid = false;
+    }
+    if ($_POST["selectAge"] == "Please Select") {
+        $_SESSION["ageErr"] = "Age is required";
+        $valid = false;
+    }
+    if (empty($_POST["checkConditions"])) {
+        $_SESSION["termsErr"] = "Terms and Conditions must be ticked";
+        $valid = false;
+    }
+    return $valid;
+}
+
+function checkConditions(): bool
+{
+    $valid = true;
+
+    $username = trimString($_POST["txtUsername"]);
+    $email = trimString($_POST["txtEmail"]);
+    $password = trimString($_POST["txtPass"]);
+
+    if (strlen($username) < 6) {
+        $_SESSION["nameErr"] = "Username must be greater than 6 characters";
+        $valid = false;
+    }
+    if (!preg_match("/^[a-zA-Z0-9]*$/",$username)) {
+        $_SESSION["nameErr"] = "No special characters allowed";
+        $valid = false;
+    }
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $_SESSION["emailErr"] = "Email is not valid";
+        $valid = false;
+    }
+    if (!preg_match("/^.*(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).*.{8,}$/", $password)) {
+        $_SESSION["passErr"] = "Password must be greater than 8 characters and have at least 1 uppercase, lowercase
+         and number ";
+        $valid = false;
+    }
+    return $valid;
+}
+
+if (checkConditions() || checkNotEmpty()) {
+    $username = $connection -> real_escape_string(stripcslashes(trim($_POST["txtUserName"])));
+    $email = $connection -> real_escape_string(stripcslashes(trim($_POST["txtEmail"])));
+    $password = $connection -> real_escape_string(md5(stripcslashes(trim($_POST["txtPass"]))));
+    $age = $connection ->real_escape_string($_POST["selectAge"]);
+
+    $query = "INSERT INTO `register`
+              (`regUser`, `regEmail`, `regPass`, `regAge`) 
+              VALUES 
+              ('$username', '$email', '$password', '$age')";
+
+    try {
+        mysqli_query($connection, $query);
+        $_SESSION["status"] = "$username you have been registered please login";
+        header("location: {$_SERVER['HTTP_REFERER']}");
+    } catch (mysqli_sql_exception) {
+        $_SESSION["status"] = "ERROR: Could not execute '$query'. " . mysqli_error($connection);
+        header("location: {$_SERVER['HTTP_REFERER']}");
+    }
+} else {
+    $_SESSION["status"] = "<span class='error'>*Could not complete registration</span>";
+    header("location: {$_SERVER['HTTP_REFERER']}");
+}
+
+?>
+</body>
+</html>
---
+++ portfolio/register/register.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Register</title>
+    <link type="text/css" rel="stylesheet" href="../../style/main.css" />
+</head>
+<header>
+    <h1>Josh Dennis c3641149</h1>
+</header>
+<body>
+<?php
+include "connection.php";
+?>
+<div>
+    <h2>Register</h2>
+    <form method="post" action="/portfolio/register/insertRecord.php">
+        <fieldset class="formFit">
+            <legend>
+                Enter Details
+            </legend>
+            <label for="regUser">username: </label>
+            <br />
+            <input type="text" name="txtUsername" id="regUser"
+                   value="<?php if (isset($_SESSION["username"])) echo $_SESSION["username"]; ?>" />
+            <span class="error"><?php if (isset($_SESSION["nameErr"]))echo "*" . $_SESSION["nameErr"];?></span>
+            <br />
+            <label for="regEmail">Email: </label>
+            <br />
+            <input type="text" name="txtEmail" id="regEmail"
+                   value="<?php if (isset($_SESSION["email"])) echo $_SESSION["email"]; ?>"/>
+            <span class="error"><?php if (isset($_SESSION["emailErr"]))echo "*" . $_SESSION["emailErr"];?></span>
+            <br />
+            <label for="regPass">Password: </label>
+            <br />
+            <input type="password" name="txtPass" id="regPass" />
+            <span class="error"><?php if (isset($_SESSION["passErr"]))echo "*" . $_SESSION["passErr"];?></span>
+            <br /><br />
+            <label for="regAge">Age: </label>
+            <br />
+            <select name="selectAge" id="regAge">
+                <option>Please Select</option>
+                <option
+                    <?php
+                    if (isset($_SESSION["age"]) && $_SESSION["age"] == "Under 18") echo "selected";
+                    ?>>Under 18
+                </option>
+                <option
+                    <?php
+                    if (isset($_SESSION["age"]) && $_SESSION["age"] == "18 to 30") echo "selected";
+                    ?>>18 to 30
+                </option>
+                <option
+                    <?php
+                    if (isset($_SESSION["age"]) && $_SESSION["age"] == "31 to 60") echo "selected";
+                    ?>>31 to 60
+                </option>
+                <option
+                    <?php
+                    if (isset($_SESSION["age"]) && $_SESSION["age"] == "Over 60") echo "selected";
+                    ?>>Over 60
+                </option>
+            </select>
+            <span class="error"><?php if (isset($_SESSION["ageErr"]))echo "*" . $_SESSION["ageErr"];?></span>
+            <br /><br />
+            <input type="checkbox" name="checkConditions" id="regConditions" value="checked"
+                <?php if (isset($_SESSION["conditions"]))echo "checked";?> />
+            <label for="regConditions">Terms and Conditions</label>
+            <span class="error"><?php if (isset($_SESSION["termsErr"]))echo "*" . $_SESSION["termsErr"];?></span>
+        </fieldset>
+        <input type="submit" name="regSubmit" id="regSubmit" />
+    </form>
+    <?php
+    if (isset($_SESSION["status"])) {
+        echo "<br />" . $_SESSION["status"];
+    }
+    $_SESSION = array();
+    ?>
+</div>
+<footer>
+    <br />
+    <small> <a href="../../html/WatIndex.html">Home</a></small>
+</footer>
+</body>
+</html>
