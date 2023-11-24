e6d6cd4234ef8e420842e4829e255a5fd3d9181c
Josh <114511121+JDennis21@users.noreply.github.com>
2023-11-24 15:43:31 +0000

    Fixed connection for php 8
    Signed-off-by: Josh <114511121+JDennis21@users.noreply.github.com>

--- crud/connection.php
+++ crud/connection.php
@@ -5,5 +5,8 @@ $username = 'root'; //your standard uni id
 $password = ''; // the password found on the W: drive
 $databaseName = 'wat2023'; //the name of the db you are using on phpMyAdmin
 
-$connection = mysqli_connect($hostname, $username, $password, $databaseName) or exit("Unable to connect to database!");
-
+try {
+    $connection = mysqli_connect($hostname, $username, $password, $databaseName);
+} catch (mysqli_sql_exception $e) {
+    $connection = exit("unable to connect to database!");
+}
