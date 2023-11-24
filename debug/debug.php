aacf30be8d63d3744051d018a330af26f1657044
Josh <114511121+JDennis21@users.noreply.github.com>
2023-11-24 15:45:13 +0000

    Debug task complete
    Signed-off-by: Josh <114511121+JDennis21@users.noreply.github.com>

---
+++ debug/debug.php
@@ -0,0 +1,25 @@
+<?php
+include 'showErrors.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link type="text/css" rel="stylesheet" href=""/>
+    <title>Result</title>
+</head>
+<body>
+<h1>Insert a record</h1>
+<form method="post" action="debugInsert.php">
+    <input type="text" name="txtName" />
+    <br />
+    <input type="text" name="txtCategory" />
+    <br />
+    <input type="submit" value="Submit" name="subEvent" />
+</form>
+<?php
+echo "<br />";
+//display results
+include 'debugSelect.php';
+?>
+</body>
+</html>
---
+++ debug/debugSelect.php
@@ -0,0 +1,14 @@
+<?php
+include 'showErrors.php';
+include 'connection.php';
+global $connection;
+
+$query="SELECT * FROM events where eventCategory='workshop'";
+
+$result=mysqli_query($connection, $query);
+
+while($row=mysqli_fetch_assoc($result)){
+    echo $row['eventName'].' '.$row['eventCategory'].'<br />';
+}
+?>
+
